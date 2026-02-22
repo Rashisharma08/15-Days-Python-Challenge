0179c7cf8fb9e39482aebdf0fa0474c50252af53
rashi sharma <148229369+Rashisharma08@users.noreply.github.com>
2026-02-22 23:52:15 +0530

    Implement print and conditional examples in day1_basics.py
    Demonstrate basic Python syntax with print and conditional statements.
    This file contains basic Python programs covering print statements and conditional statements (if, else, elif) as part of my 15 Days Python Challenge.

---
+++ day1_basics.py
@@ -0,0 +1,23 @@
+# Day 1 - Python Basics
+# Topics: print(), if, else, elif
+
+# 1. Print Example
+print("Hello Python")
+
+# 2. Simple If-Else
+num = 7
+
+if num > 10:
+    print("Big")
+else:
+    print("Small")
+
+# 3. If-Elif-Else Example
+num = 10
+
+if num > 10:
+    print("Big")
+elif num == 10:
+    print("Equal")
+else:
+    print("Small")
