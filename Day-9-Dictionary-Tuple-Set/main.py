04dd0f123dd3c6bd2d254f79f9f27e957524e340
rashi sharma <148229369+Rashisharma08@users.noreply.github.com>
2026-03-31 15:22:40 +0530

    Add dictionary, tuple, and set examples in main.py

---
+++ Day-9-Dictionary-Tuple-Set/main.py
@@ -0,0 +1,17 @@
+# Dictionary
+student = {
+    "name": "Rashi",
+    "age": 20,
+    "city": "Bhopal"
+}
+
+for key, value in student.items():
+    print(key, ":", value)
+
+# Tuple
+data = ("Rashi", 20, "Data Science")
+print(data[0])
+
+# Set
+subjects = {"Python", "AI", "Python", "ML"}
+print(subjects)
